0a3aa55ababa7bdf2438d25946eb09538c0a496d
Elijah Brown <elijahbrown737@gmail.com>
2020-10-19 22:25:15 -0700

    nav

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import routes from './routes'
 import {HashRouter, Link} from 'react-router-dom'
 import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Yo Momma
-          </p>
-          <LambdaDemo />
           <HashRouter>
-          <Link to='/2'>22222</Link>
-          <Link to='/'>HOME</Link>
+          <div className="nav">
+          <Link className="Link" to='/'>HOME</Link>
+          <Link className="Link" to='/2'>22222</Link>
+          <Link className="Link" to='/LD'>Lambda Demo</Link>
+          </div>
           {routes}
           </HashRouter>
         </header>
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import Page from './components/Page'
 import Page2 from './components/Page2'
+import LambdaDemo from './components/LambdaDemo'
 
 export default (
     <Switch>
         <Route exact path='/' component={Page} />
         <Route path='/2' component={Page2} />
+        <Route path='/LD' component={LambdaDemo} />
     </Switch>
 );
