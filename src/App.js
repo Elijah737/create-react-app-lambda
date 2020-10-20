d4ca28b24d05167451b7c1410860cf1ebc3c2b14
Elijah Brown <elijahbrown737@gmail.com>
2020-10-20 02:02:25 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {HashRouter, Link} from 'react-router-dom'
 import "./App.css"
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <HashRouter>
           <div className="nav">
-            <Link className="Link" to='/'>HOME</Link>
+            <Link onClick={something()} className="Link" to='/'>HOME</Link>
             <Link className="Link" to='/2'>22222</Link>
             <Link className="Link" to='/LD'>Lambda Demo</Link>
           </div>
