02fbf96d8267e4ff3421e21a8ad0c9df14dcc913
Elijah Brown <elijahbrown737@gmail.com>
2020-10-21 22:48:38 -0700

    icons added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -12,15 +12,21 @@ const Resume = () => {
                 <p>I am a new developer who is eager to learn and grow. I am a recent graduate from  
                     <span><a href="https://devmountain.com"> DevMountain</a></span>, 
                     where I learned full stack web development with React.
-                    My talent seems to be in front-end development as I have worked in graphic design previously and have been a life-long artist. 
+                    My talent seems to be in front-end development as I have worked in graphic design previously and am a life-long artist. 
                     I am a prior Marine and have a Associates degree in Culinary Arts. 
                     I worked as a Chef for over 10 years. Now I am transitioning to a new and promising career in tech. 
                     I am very excited to begin this journey and see where it takes me!</p>
             </div>
         </div>
     </div>
-
-
+    <div className="technologies">
+        <div className="icon icon1"></div>
+        <div className="icon icon2"></div>
+        <div className="icon icon3"></div>
+        <div className="icon icon4"></div>
+        <div className="icon icon5"></div>
+    </div>
+    <a href="https://iconscout.com/icons/react" target="_blank">Icons</a> by <a href="https://iconscout.com/contributors/unicons" target="_blank">Unicons Font</a>
     </div> );
 }
  
